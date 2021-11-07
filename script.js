7decd574bcb4b980a1bc40d4e548b7d0f3828590
arzuashik <arzu2050@gmail.com>
2021-11-07 16:34:05 +0600

    using dummy bond

---
+++ script.js
@@ -0,0 +1,21 @@
+const form = document.getElementById("draw-form");
+const formInput = document.getElementById("bond-numbers");
+const loadedBonds = document.querySelector(".loaded-bonds");
+
+function loadBond(){
+    formInput.value = myBonds.join(",");
+    myBonds.forEach(bond => {
+        const div = document.createElement("div");
+        div.classList.add("bond");
+        div.innerHTML = `<h2>${bond}</h2>`;
+        loadedBonds.appendChild(div);
+    });
+}
+function search(){
+    if(formInput.value !== ""){
+        form.submit();
+    }
+}
+function addBond(){
+
+}
